142b60f8019c7bee3f4026a36444529b3d258a9d
SKORP-ION <alex.tulski@gmail.com>
2021-05-18 10:10:56 +0300

    Создан генератор случайной последовательности числел

---
+++ utils/workstations.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func GeneratePassword() string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+	length := 64
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	str := b.String()
+	return str
+}
