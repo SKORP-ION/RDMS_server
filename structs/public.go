ec51f3d8822c90a0fb602e71d92cfac868b4e3b4
SKORP-ION <alex.tulski@gmail.com>
2021-05-18 10:11:27 +0300

    Разбил структуры на public и private.

---
+++ structs/public.go
@@ -0,0 +1,13 @@
+package structs
+
+type PublicWorkstation struct {
+	Name string `gorm:"colummn:name" json:"name"`
+	Serial string `gorm:"column:serial_number"`
+	Personal_key string `gorm:"column:personal_key"`
+}
+func (pws PublicWorkstation) FromWs(ws Workstation) PublicWorkstation {
+	pws.Name = ws.Name
+	pws.Serial = ws.Serial
+	pws.Personal_key = ws.Personal_key
+	return pws
+}
